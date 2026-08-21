99425661a016ebe8efcf880cc94dd0e9f34991a2
FlameyosSnowy <flamesfflowsnow@gmail.com>
2026-08-21 12:23:19 +0300

    Make it ready for when I add windows and macos

--- zeta-compiler-api/src/file_loader.rs
+++ zeta-compiler-api/src/file_loader.rs
@@ -1,6 +1,13 @@
 use std::path::PathBuf;
 
+#[cfg(target_os = "linux")]
 use crate::io_uring_file_loader::IoUringFileLoader;
+
+#[cfg(target_os = "windows")]
+use crate::iocp_file_loader::IocpFileLoader;
+
+#[cfg(not(any(target_os = "linux", target_os = "windows")))]
+use crate::std_file_loader::StdFileLoader;
 
 pub trait FileLoader {
     fn load_files(&self, paths: &[PathBuf]) -> Result<Vec<SourceFile>, std::io::Error>;
@@ -13,5 +20,13 @@ pub struct SourceFile {
 }
 
 pub fn choose_file_loader() -> impl FileLoader {
-    IoUringFileLoader::new(256)
+    #[cfg(target_os = "linux")]
+    {
+        return IoUringFileLoader::new(256);
+    }
+
+    #[cfg(not(any(target_os = "linux", target_os = "windows")))]
+    {
+        return StdFileLoader;
+    }
 }
